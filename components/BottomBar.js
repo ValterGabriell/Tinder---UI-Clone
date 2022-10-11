689a5475d2a18fc14887db6e5d7734f81752ccdb
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2022-10-10 21:26:46 -0300

    finalizando

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -1,49 +1,43 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
-function BottomBar() {
-    return (
-        <View style={styles.container}>
-            <View/>
-            <TouchableOpacity style={styles.button}>
-                <FontAwesome name='times' size={27} color="#f06795"></FontAwesome>
-            </TouchableOpacity>
 
-            <TouchableOpacity style={styles.button}>
-                <FontAwesome name='heart' size={27} color="#64edcc"></FontAwesome>
-            </TouchableOpacity>
-            <View/>
-        </View>
-
-
-    )
+export default function BottomBar({ handleLikePress, handlePassPress }) {
+  return (
+    <View style={styles.container}>
+      <View />
+      <TouchableOpacity style={styles.button} onPress={handlePassPress}>
+        <FontAwesome name="times" size={27} color="#F06795"></FontAwesome>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <FontAwesome name="heart" size={27} color="#64EDCC" onPress={handleLikePress}></FontAwesome>
+      </TouchableOpacity>
+      <View />
+    </View>
+  )
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        height: 75,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        alignItems: "center"
+  container: {
+    height: 75,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  button: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 0,
     },
-    button: {
-        width: 50,
-        height: 50,
-        backgroundColor: "white",
-        borderRadius: 25,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 0
-        },
-        shadowOpacity: 0.15,
-        shadowRadius: 6.46,
-        elevation: 9
-    }
-});
-
-
-export default BottomBar
+    shadowOpacity: 0.15,
+    shadowRadius: 6.46,
+    elevation: 9,
+  },
+})
